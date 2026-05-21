f33ef64443108037e2df47b40c4dd86b1eedea82
mr28Verma <2006sakshamchd@gmail.com>
2026-05-21 18:27:58 +0530

    Final commit

--- config/sendEmail.js
+++ config/sendEmail.js
@@ -1,23 +1,29 @@
 const nodemailer = require("nodemailer");
 
 const transport = nodemailer.createTransport({
-    secure: true,
-    host: 'smtp.gmail.com',
-    port:465,
-    auth:{
+    host: "smtp-relay.brevo.com",
+    port: 587,
+    secure: false,
+    auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS
     }
-})
+});
 
-async function sendMail(to,sub,msg){
-   return transport.sendMail({
-        from: process.env.MAIL_USER,
-        to: to,
-        subject: sub,
-        html: msg
-    })
-    
+async function sendMail(to, sub, msg) {
+    try {
+        const info = await transport.sendMail({
+            from: process.env.MAIL_USER,
+            to: to,
+            subject: sub,
+            html: msg
+        });
+
+        console.log("Mail Sent:", info.messageId);
+
+    } catch (error) {
+        console.log("Mail Error:", error);
+    }
 }
 
-module.exports = {sendMail}
+module.exports = { sendMail };
